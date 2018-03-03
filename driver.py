c7d949f2205eecb5d7bed3093d91b4ac19da7e03
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-03-02 20:53:31 -0500

    Initial commit

---
+++ driver.py
@@ -0,0 +1,90 @@
+import os
+from datetime import datetime
+
+import easygui
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def is_valid_log(file):
+    fields = file.dtype.fields.keys()
+
+    return all(key in fields for key in (
+        "Time", "xActual", "yActual", "angleActual", "xTarget", "yTarget", "angleTarget", "XTE", "lagE", "angleE",
+        "pLeft",
+        "pRight"))
+
+
+def plot_graphs(csv_files):
+    fig, axs = plt.subplots(ncols=len(csv_files), nrows=3)
+
+    if len(csv_files) == 1:
+        get_axis = lambda x, index: x
+    else:
+        get_axis = lambda x, index: x[:, index]
+
+    for i, date in enumerate(sorted(csv_files, reverse=True)):
+        path, errors, powers = get_axis(axs, i)
+        current_file = csv_files[date]
+
+        path.set_title(date)
+        path.plot(current_file["xActual"], current_file["yActual"], c="r", label="Actual")
+        path.scatter(current_file["xActual"][[0, -1]], current_file["yActual"][[0, -1]], c="r")
+
+        path.plot(current_file["xTarget"], current_file["yTarget"], c="b", label="Target")
+        path.scatter(current_file["xTarget"][[0, -1]], current_file["yTarget"][[0, -1]], c="b")
+
+        path.grid(True)
+        handles, labels = path.get_legend_handles_labels()
+        path.legend(handles, labels)
+
+        errors.plot(current_file["XTE"], c="r", label="???")
+        errors.plot(current_file["lagE"], c="g", label="Lag Error")
+        errors.plot(current_file["angleE"], c="b", label="Angle Error")
+        errors.grid(True)
+        handles, labels = errors.get_legend_handles_labels()
+        errors.legend(handles, labels)
+        errors.set_xlim(0)
+
+        powers.plot(current_file["pLeft"], c="r", label="Left Power")
+        powers.plot(current_file["pRight"], c="b", label="Left Power")
+        powers.grid(True)
+        handles, labels = powers.get_legend_handles_labels()
+        powers.legend(handles, labels)
+        powers.set_xlim(0)
+
+    plt.show()
+
+
+def main():
+    openPath = "{0:s}\*.csv".format(os.path.expanduser("~"))
+
+    while True:
+        print(openPath)
+
+        files = easygui.fileopenbox('Please locate csv files', 'Specify File', default=openPath, filetypes='*.csv',
+                                    multiple=True)
+
+        if files:
+            openPath = "{0:s}\*.csv".format(os.path.dirname(files[0]))
+
+            csv_files = {}
+
+            for file in files:
+                file_data = np.genfromtxt(file, delimiter=',', dtype=np.float32, names=True)
+
+                if is_valid_log(file_data):
+                    date = datetime.strptime(os.path.basename(file), "%Y-%m-%d %H-%M-%S.csv")
+                    csv_files[date] = file_data
+                else:
+                    easygui.msgbox(
+                        "The file {0:s} is not a valid file it will be removed.".format(os.path.basename(file)))
+
+            plot_graphs(csv_files)
+
+        else:
+            break
+
+
+if __name__ == '__main__':
+    main()
