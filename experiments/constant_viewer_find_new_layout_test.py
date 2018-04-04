227354a5274bb27496f99ffb128ab488c132d1e4
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-04-04 13:17:10 -0400

    tested new layout

--- experiments/constant_viewer_find_new_layout_test.py
+++ experiments/constant_viewer_find_new_layout_test.py
@@ -1,48 +1,54 @@
-import sklearn
-from itertools import combinations
-
 import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 from mpl_toolkits.mplot3d import Axes3D
-from sklearn import ensemble
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 
-from visualize.helper import plot_hyperplane
+from visualize.helper import plot_hyperplane, plot_subplots
 
-fig = plt.figure()
-gs = GridSpec(3, 4, fig)
+if __name__ == '__main__':
+    fig = plt.figure()
+    gs = GridSpec(3, 4, fig)
 
-from sklearn.datasets import make_classification
+    from sklearn.datasets import make_classification
 
-features, labels = make_classification(n_features=3, n_informative=2, n_redundant=1, n_clusters_per_class=1)
+    # 0 == velocity
+    # 1 == time
+    # 2 == power
 
+    features, labels = make_classification(n_features=3, n_informative=2, n_redundant=1, n_clusters_per_class=1)
 
-def plot(features, lables, subplots):
-    velocty, time, power = features[:, 0], features[:, 1], features[:, 2]
+    # Axes3D
+    master_plot = fig.add_subplot(gs[:3, :3], projection='3d')
+    master_plot.set_xlabel("Velocity")
+    master_plot.set_ylabel("Time")
+    master_plot.set_zlabel("Power")
+    time_velocity = fig.add_subplot(gs[0, -1])
+    time_power = fig.add_subplot(gs[1, -1])
+    power_velocity = fig.add_subplot(gs[2, -1])
 
-    for combination, subplot in zip(combinations((velocty, time, power), 2), subplots):
-        subplot.scatter(combination[0], combination[1], c=labels)
+    color_label = list(map(lambda x: 'r' if x == 1 else 'b', labels))
 
-# Axes3D
-master_plot = fig.add_subplot(gs[:3, :3], projection='3d')
-time_velocity = fig.add_subplot(gs[0, -1])
-time_power = fig.add_subplot(gs[1, -1])
-power_velocity = fig.add_subplot(gs[2, -1])
+    master_plot.scatter(features[:, 0], features[:, 1], features[:, 2], c=color_label)
 
-master_plot.scatter(features[:, 0], features[:, 1], features[:, 2], c=labels)
+    plot_subplots(features, {0: 'Average Power', 1: 'Velocity', 2: 'Time'}, (time_velocity, power_velocity, time_power),
+                  labels=color_label)
 
-plot(features, labels, (time_velocity, time_power, power_velocity))
+    clf = SVC(kernel="linear", gamma=10)
+    # clf = sklearn.ensemble.BaggingClassifier()
+    clf.fit(features, labels)
 
-clf= SVC(kernel="rbf", C=10)
-# clf = sklearn.ensemble.BaggingClassifier()
-clf.fit(features, labels)
+    plot_hyperplane(clf, master_plot, interval=.05)
 
-plot_hyperplane(clf, master_plot, interval=.05)
+    gs.tight_layout(fig)
 
-gs.tight_layout(fig)
+    figManager = plt.get_current_fig_manager()
+    figManager.window.showMaximized()
 
-figManager = plt.get_current_fig_manager()
-figManager.window.showMaximized()
+    # plt.show()
+    fig.show()
+    plt.show()
 
-plt.show()
+    # fig.clear()
+    Axes3D(fig)
+    fig.show()
+    plt.show(fig)
