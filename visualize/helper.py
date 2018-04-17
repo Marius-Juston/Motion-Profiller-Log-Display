8d7be6697252f92326e9905d58dfb792987dcc81
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-04-17 16:22:09 -0400

    updated helper to include showinig line of best fit 2d and 3d and 2d and 3d hyperplane

--- visualize/helper.py
+++ visualize/helper.py
@@ -3,8 +3,10 @@ import math
 from datetime import datetime
 from itertools import combinations
 
+import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.axes import Axes
+from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Line3DCollection
 from skimage import measure
 from sklearn.exceptions import NotFittedError
@@ -25,10 +27,14 @@ def find_linear_best_fit_line(x, y):
 
 
 def plot_subplots(features, translation, subplots, labels=None):
-    for combination, subplot in zip(combinations(list(_ for _ in range(features.shape[1])), 2), subplots):
+    variable_combinations = combinations(list(_ for _ in range(features.shape[1])), 2)
+
+    for combination, subplot in zip(variable_combinations, subplots):
         subplot.scatter(features[:, combination[1]], features[:, combination[0]], c=labels)
         subplot.set_xlabel(translation[combination[1]])
         subplot.set_ylabel(translation[combination[0]])
+
+    return variable_combinations
 
 
 def rotate_points_around_point(points: np.ndarray, angle: float, point: iter = (0, 0)) -> np.ndarray:
@@ -58,50 +64,90 @@ Retrieves the data from the csv log file.
     return np.genfromtxt(file, delimiter=DELIMITER, dtype=DTYPE, names=True, encoding=ENCODING)
 
 
-def plot_hyperplane(clf, ax, interval=.1):
+def plot_hyperplane(clf, ax: Axes = None, interval: float = .05, alpha=.3,
+                    colors = ('r', 'b')) -> Line3DCollection:
     """
 Plots the hyperplane of the model in an axes
     :param clf: the classifier to use to find the hyperplane
     :param ax: the axes to plot the hyperplane into
     :param interval: the precision of the the hyperplane rendering.
+    :param alpha:
+    :param colors:
     :return: the mesh of the created hyperplane that was added to the axes
     """
+
+    is_3d = False
+
+    if ax is None:
+        try:
+            clf.predict([[0, 0, 0]])
+            is_3d = True
+            ax = plt.gca(projection="3d")
+        except ValueError:
+            is_3d = False
+            ax = plt.gca()
+
+    elif isinstance(ax, Axes3D):
+        is_3d = True
 
     interval = int(1 / interval)
 
     # get the separating hyperplane
     x_min, x_max = ax.get_xlim()
     y_min, y_max = ax.get_ylim()
-    z_min, z_max = ax.get_zlim()
 
     # create grid to evaluate model
     xx = np.linspace(x_min, x_max, interval)
     yy = np.linspace(y_min, y_max, interval)
-    zz = np.linspace(z_min, z_max, interval)
 
-    yy, xx, zz = np.meshgrid(yy, xx, zz)
+    if is_3d:
+        z_min, z_max = ax.get_zlim()
 
-    if hasattr(clf, "decision_function"):
-        z = clf.decision_function(np.c_[xx.ravel(), yy.ravel(), zz.ravel()])
-    elif hasattr(clf, "predict_proba"):
-        z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel(), zz.ravel()])[:, 1]
+        zz = np.linspace(z_min, z_max, interval)
+
+        yy, xx, zz = np.meshgrid(yy, xx, zz)
+
+        if hasattr(clf, "decision_function"):
+            z = clf.decision_function(np.c_[xx.ravel(), yy.ravel(), zz.ravel()])
+        elif hasattr(clf, "predict_proba"):
+            z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel(), zz.ravel()])[:, 1]
+        z = z.reshape(xx.shape)
+
+        vertices, faces, _, _ = measure.marching_cubes(z, 0)
+        # Scale and transform to actual size of the interesting volume
+        vertices = vertices * [x_max - x_min, y_max - y_min, z_max - z_min] / interval
+        vertices += [x_min, y_min, z_min]
+        # and create a mesh to display
+        mesh = Line3DCollection(vertices[faces],
+                                facecolor=colors, alpha=alpha)
+
+        ax.add_collection3d(mesh)
+
+        return mesh
     else:
-        # TODO  find out how to find the hyperplanes for unsupervised learning algorithms
-        z = clf.transform(np.c_[xx.ravel(), yy.ravel(), zz.ravel()])
-    z = z.reshape(xx.shape)
+        xx, yy = np.meshgrid(xx,
+                             yy)
+        Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
 
-    vertices, faces, _, _ = measure.marching_cubes(z, 0)
-    # Scale and transform to actual size of the interesting volume
-    vertices = vertices * [x_max - x_min, y_max - y_min, z_max - z_min] / interval
-    vertices += [x_min, y_min, z_min]
-    # and create a mesh to display
-    # mesh = Poly3DCollection(vertices[faces],
-    #                         facecolor='orange', alpha=0.3)
-    mesh = Line3DCollection(vertices[faces],
-                            facecolor='orange', alpha=0.3)
-    ax.add_collection3d(mesh)
+        return ax.contourf(xx, yy, Z, 10, colors=colors, alpha=alpha)
 
-    return mesh
+
+def plot_fitting_plane(clf, ax, number: int = 50, color=None):
+    x_min, x_max = ax.get_xlim()
+
+    if isinstance(ax, Axes3D):
+        y_min, y_max = ax.get_ylim()
+        yy, xx = np.meshgrid(np.linspace(y_min, y_max, number), np.linspace(x_min, x_max, number))
+        z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+        z = z.reshape(xx.shape)
+
+        return ax.plot_wireframe(xx, yy, z, facecolors=color)
+    else:
+        x = np.linspace(x_min, x_max, number)
+        y = clf.predict(x)
+
+        return ax.plot(x, y, c=color)
 
 
 def get_velocity(time: np.ndarray, file_data: np.ndarray, actual: bool = True) -> np.ndarray:
@@ -227,8 +273,6 @@ Finds the largest factor of x
     return int(i)
 
 
-
-
 def set_visible(patches: iter, value: bool) -> None:
     """
 Sets the matplotlib patch visibility to be either on or off.
@@ -292,7 +336,8 @@ Shows the legends for the Axes
     for subplot in args:
         handles, labels = subplot.get_legend_handles_labels()
         subplot.legend(handles, labels)
-        
+
+
 def retrieve_parameters(clf):
     parameters = {}
 
