ff002ccb48e5e5cc456bb561b9b8a31918063abe
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-10-19 18:10:52 -0400

    updated experiments
    added in unison vetical line experiment

---
+++ experiments/LineAnimation.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.gridspec import GridSpec
+
+fig = plt.gcf()
+gs = GridSpec(3, 1, fig)
+
+velocity = fig.add_subplot(gs[0])
+errors = fig.add_subplot(gs[1])
+powers = fig.add_subplot(gs[2])
+
+line = velocity.axvline(x=0)
+line2 = errors.axvline(x=0)
+line3 = powers.axvline(x=0)
+
+def update_line(i):
+    x = 1 / (i + 1)
+
+    line.set_xdata(x)
+    line2.set_xdata(x)
+    line3.set_xdata(x)
+    print("Hello")
+
+    return [ line, line2, line3]
+
+
+animation = FuncAnimation(fig, update_line,
+                          blit=True,
+                          repeat=False)
+
+plt.show()
--- experiments/straight_line_testing.py
+++ experiments/straight_line_testing.py
@@ -1,10 +1,13 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from visualize.helper import is_straight_line
 
-x = np.array([2, 4, 0, -4, 6])
-y = np.array([3, 4, 2, 0, 5])
+x = np.array([-4, -2, 0, 4, 2])
+y = np.array([1, 2, 3, 4, 5])
 
 data = dict(xTarget=x, yTarget=y)
 
+plt.scatter(x, y, c=['r', 'b', 'g', 'indigo', 'magenta'])
 print(is_straight_line(data))
+plt.show()
