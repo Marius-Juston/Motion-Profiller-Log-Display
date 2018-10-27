57f70facbfb1129179d4bd46b5b57f1afe9f6357
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-10-27 10:41:51 -0400

    started data cleanup process

--- experiments/new_model_trainer.py
+++ experiments/new_model_trainer.py
@@ -8,7 +8,27 @@ from sklearn.preprocessing import MinMaxScaler
 import visualize
 from visualize import LEGACY_COLUMNS
 from visualize.helper import is_valid_log, get_data, get_features
+from visualize.new_constant_viewer import manipulate_features
 from visualize.new_selector import Graphs, LassoManager
+
+
+def go_through_process(all_features: np.ndarray, all_data: np.ndarray):
+    graphs = Graphs(all_features, title="Outlier Selection",
+                    suptitle="Please unselect the outliers. Press [Enter] to confirm.")
+    selector = LassoManager(graphs)
+    plt.show()
+
+    all_features = all_features[selector.indexes]
+    all_data = all_data[selector.indexes]
+
+    new_scaled_features, outliers, features = manipulate_features(all_features, all_data)
+
+    all_features = features
+
+    graphs = Graphs(all_features, title="Acceleration vs Deceleration Selection",
+                    suptitle="Selected=accelerating, Unselected=decelerating. Press [Enter] to confirm.")
+    selector = LassoManager(graphs)
+    plt.show()
 
 
 def train_model(open_path):
@@ -27,6 +47,7 @@ def train_model(open_path):
             open_path = "{0:s}\*.csv".format(os.path.dirname(files[0]))
 
             all_features = None
+            all_data = None
 
             for file in files:
                 file_data = get_data(file)
@@ -40,19 +61,15 @@ def train_model(open_path):
 
                     if all_features is None:
                         all_features = features
+                        all_data = file_data
                     else:
                         all_features = np.concatenate((all_features, features))
+                        all_data = np.concatenate((all_data, file_data))
                 else:
                     easygui.msgbox(
                         "The file {0:s} is not a valid file, it will not be plotted.".format(os.path.basename(file)))
 
-            # time = MinMaxScaler().fit_transform(all_features[:, 2].reshape(-1, 1))
-            #
-            # all_features[:, 2] = time.reshape(1, -1)
-
-            grpahs = Graphs(all_features)
-            selector = LassoManager(grpahs)
-            plt.show()
+            go_through_process(all_features, all_data)
         else:
             break
 
