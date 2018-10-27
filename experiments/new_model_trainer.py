25905d90f7f389eaf3bafdebffe247731e0f4e1c
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-10-27 10:05:53 -0400

    started working on improved version of the model trainer

--- experiments/new_model_trainer.py
+++ experiments/new_model_trainer.py
@@ -1,10 +1,14 @@
 import os
 
 import easygui
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
 import visualize
-from visualize.helper import get_features, is_valid_log, get_data
-from visualize.processing import OutlierAndScalingSelection
+from visualize import LEGACY_COLUMNS
+from visualize.helper import is_valid_log, get_data, get_features
+from visualize.new_selector import Graphs, LassoManager
 
 
 def train_model(open_path):
@@ -16,70 +20,42 @@ def train_model(open_path):
     # TODO x = motor power, y velocity, z time
 
     while True:
-        file = easygui.fileopenbox('Please locate csv file', 'Specify File', default=open_path, filetypes='*.csv')
+        files = easygui.fileopenbox('Please locate csv file', 'Specify File', default=open_path, filetypes='*.csv',
+                                    multiple=True)
 
-        if file:
-            open_path = "{0:s}\*.csv".format(os.path.dirname(file))
+        if files:
+            open_path = "{0:s}\*.csv".format(os.path.dirname(files[0]))
 
-            file_data = get_data(file)
+            all_features = None
 
-            # TODO make this loop thought the steps as many times as they are number of paths
-            if is_valid_log(file_data, visualize.LEGACY_COLUMNS):
-                x, _ = get_features(file_data)
+            for file in files:
+                file_data = get_data(file)
 
-                outlier = OutlierAndScalingSelection(file_data, x)
-                outlier.show()
+                if is_valid_log(file_data, LEGACY_COLUMNS):
+                    features, _ = get_features(file_data)
 
-                del outlier
+                    time = MinMaxScaler().fit_transform(features[:, 2].reshape(-1, 1))
 
+                    features[:, 2] = time.reshape(1, -1)
+
+                    if all_features is None:
+                        all_features = features
+                    else:
+                        all_features = np.concatenate((all_features, features))
+                else:
+                    easygui.msgbox(
+                        "The file {0:s} is not a valid file, it will not be plotted.".format(os.path.basename(file)))
+
+            # time = MinMaxScaler().fit_transform(all_features[:, 2].reshape(-1, 1))
+            #
+            # all_features[:, 2] = time.reshape(1, -1)
+
+            grpahs = Graphs(all_features)
+            selector = LassoManager(grpahs)
+            plt.show()
         else:
             break
 
-    # total_data = {}
-    # already_used_files = set()
-    # changed_anything = False
-    # hyperplane = None
-    #
-    # if os.path.exists(MODEL_FILE):
-    #     answer = easygui.boolbox("A model already exists do you wish to use it?")
-    #
-    #     if answer is None:
-    #         return
-    #
-
-    # get_features()  # elif answer:
-    #         clf = joblib.load(MODEL_FILE)
-    #         # hyperplane = plot_hyperplane(clf, ax3d)
-    #         data = np.load(MODEL_DATA_FILE)
-    #         total_data["features"] = data["features"]
-    #         total_data["labels"] = data["labels"]
-    #
-    #         accelerating = total_data["features"][total_data["labels"] == 0]
-    #         decelerating = total_data["features"][total_data["labels"] == 1]
-    #
-    #         ax3d.scatter(accelerating[:, 0], accelerating[:, 1], accelerating[:, 2], c="red",
-    #                      label="acceleration")
-    #         ax3d.scatter(decelerating[:, 0], decelerating[:, 1], decelerating[:, 2], c="blue",
-    #                      label="deceleration")
-    #
-    #         already_used_files.add(*data["files"])
-    #
-    #         plt.show()
-    #     else:
-    #         clf = create_blank_classifier()
-    #         changed_anything = True
-    # else:
-    #     clf = create_blank_classifier()
-    #
-    # if changed_anything and not is_empty_model(clf):
-    #     joblib.dump(clf, MODEL_FILE)
-    #     np.savez(MODEL_DATA_FILE, features=total_data["features"], labels=total_data["labels"],
-    #              files=total_data["files"])
-    #     easygui.msgbox("Model saved.")
-    #
-    # plt.close("all")
-    # return open_path
-
 
 if __name__ == '__main__':
-    train_model("C:")
+    train_model(open_path=visualize.open_path)
